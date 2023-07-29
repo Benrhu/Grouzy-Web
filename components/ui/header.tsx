95a97c5bc8586c33e8413005aa089dd181474215
rubdh <rubdh89@proton.me>
2023-07-29 21:17:46 +0200

    website redesigned

---
+++ components/ui/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-center bg-white p-4">
+      <Link href="/">
+        <div>
+          <Image src="/images/grouzy.png" alt="Logo" width={550} height={550} />
+        </div>
+      </Link>
+    </nav>
+  );
+}
---
+++ components/ui/logo.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Logo() {
+  return (
+    <Link href="/" className="block" aria-label="Cruip">
+<div>
+  <img src="../../public/images/features-bg.png" alt="" />
+</div>
+    </Link>
+  )
+}
